a3f45b948b48eadbed5b8d1cb764048b6f5c4752
Aleksandra Bobrova <bobrova.aleksand@gmail.com>
2016-07-31 22:42:39 +0300

    Added overlay when locked

--- app/src/main/java/com/bobrusha/android/yandex/call_notification/CallReceiver.java
+++ app/src/main/java/com/bobrusha/android/yandex/call_notification/CallReceiver.java
@@ -3,8 +3,16 @@ package com.bobrusha.android.yandex.call_notification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 /**
  * Created by Aleksandra on 31/07/16.
@@ -25,6 +33,7 @@ public class CallReceiver extends BroadcastReceiver {
             if (phoneState.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
                 String phoneNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
                 incomingCall = true;
+                showWindow(context, phoneNumber);
                 Log.d(DEBUG_TAG, "Show window:" + phoneNumber);
 
             } else if (phoneState.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
@@ -38,6 +47,43 @@ public class CallReceiver extends BroadcastReceiver {
                     incomingCall = false;
                 }
             }
+        }
+    }
+
+    private static WindowManager windowManager;
+    private static ViewGroup windowLayout;
+
+    private void showWindow(Context context, String phone) {
+        windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED,
+                PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.TOP;
+
+        windowLayout = (ViewGroup) layoutInflater.inflate(R.layout.info_view, null);
+
+        TextView textViewNumber = (TextView) windowLayout.findViewById(R.id.textViewNumber);
+        ImageButton buttonClose = (ImageButton) windowLayout.findViewById(R.id.buttonClose);
+        textViewNumber.setText(phone);
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                closeWindow();
+            }
+        });
+
+        windowManager.addView(windowLayout, params);
+    }
+
+    private void closeWindow() {
+        if (windowLayout != null) {
+            windowManager.removeView(windowLayout);
+            windowLayout = null;
         }
     }
 }
--- app/src/main/java/com/bobrusha/android/yandex/call_notification/MainActivity.java
+++ app/src/main/java/com/bobrusha/android/yandex/call_notification/MainActivity.java
@@ -1,16 +1,40 @@
 package com.bobrusha.android.yandex.call_notification;
 
 import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import static android.os.Build.VERSION;
+import static android.os.Build.VERSION_CODES;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String DEBUG_TAG = MainActivity.class.getName();
+
+    private static int OVERLAY_PERMISSION_REQ_CODE = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 0);
+        if (VERSION.SDK_INT >= VERSION_CODES.M) {
+            Intent myIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+            startActivityForResult(myIntent, OVERLAY_PERMISSION_REQ_CODE);
+        }
+    }
+
+    @TargetApi(VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (!Settings.canDrawOverlays(this)) {
+                Log.d(DEBUG_TAG, "no ");
+            }
+        }
     }
 }
