734c5208aad4f6e3dba5073e5af4dd30fe60dccd
Aleksandra Bobrova <bobrova.aleksand@gmail.com>
2016-08-01 16:39:16 +0300

    Minor improvements

--- app/src/main/java/com/bobrusha/android/yandex/call_notification/MainActivity.java
+++ app/src/main/java/com/bobrusha/android/yandex/call_notification/MainActivity.java
@@ -3,8 +3,11 @@ package com.bobrusha.android.yandex.call_notification;
 import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -21,9 +24,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 0);
-        if (VERSION.SDK_INT >= VERSION_CODES.M) {
-            Intent myIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+        if (!hasPermission(Manifest.permission.READ_PHONE_STATE)) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 0);
+        }
+        if (VERSION.SDK_INT >= VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            Intent myIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
             startActivityForResult(myIntent, OVERLAY_PERMISSION_REQ_CODE);
         }
     }
@@ -36,5 +42,13 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(DEBUG_TAG, "no ");
             }
         }
+    }
+
+    public boolean hasPermission(@NonNull String... permissions) {
+        for (String p : permissions) {
+            if (ActivityCompat.checkSelfPermission(this, p) != PackageManager.PERMISSION_GRANTED)
+                return false;
+        }
+        return true;
     }
 }
